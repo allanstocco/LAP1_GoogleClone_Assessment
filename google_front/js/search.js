a2dad0524152fffb8c13914f5b90e82dc62748a1
allanstocco <allanstocco@hotmail.com>
2022-06-22 15:05:52 +0100

    Finished dark theme on first page

--- google_front/js/search.js
+++ google_front/js/search.js
@@ -1,34 +1,32 @@
-async function fetchData(){
+const searchResultArea = document.querySelector('#searchresultsarea');
+console.log(searchResultArea)
+
+async function fetchData() {
     let response = await fetch('http://0.0.0.0:3000/search');
     let data = await response.json();
     console.log(data);
     appendResults(data);
 }
 
-function appendResults(data){
+function appendResults(data) {
     data.forEach(appendResult)
 }
 
-function appendResult(itemData){
+function appendResult(itemData) {
 
-    const searchResultArea = document.querySelector('#searchresultsarea');
-
-    const container = document.createElement('div');
-    const header = document.createElement('h2');
-    const link = document.createElement('a');
-    const text = document.createElement('p');
-    container.setAttribute('class', 'searchresult');
-    container.setAttribute('id', `searchresult-${itemData.id + 1}`)
-
-    container.appendChild(header);
-    container.appendChild(link);
-    container.appendChild(text);
-
-    searchResultArea.append(container);
-
+    searchResultArea.innerHTML += `
+        <div class="searchresult">
+            <a href="${itemData.url}">${itemData.url} <button style="font-size:20px;">&#8595</button>
+                <h2>Lock ${itemData.header}</h2>
+            </a> 
+            
+            <br>
+            <p>${itemData.info}</p>
+        </div>
+    `
 }
 
-function getRandomResult(e){
+function getRandomResult(e) {
     e.preventDefault();
 }
 
--- google_front/js/styles.js
+++ google_front/js/styles.js
@@ -1,18 +1,48 @@
 const themeColor = document.querySelector('body');
 const allDivs = document.getElementsByTagName('a');
 const changeMode = document.querySelector('#changeMode');
+const searchButton = document.querySelector('.search-button');
+const randomButton = document.querySelector('.random-button');
 const footer = document.querySelector('.upperFooter');
+const footerDiv = document.querySelector('.location');
 const svg = document.querySelector('#svg');
 
 changeMode.addEventListener('click', (e) => {
-    console.log(e)
-    themeColor.style.backgroundColor = 'black'
-    themeColor.style.color = 'whitesmoke'
-    footer.style.backgroundColor = 'black'
-    svg.setAttribute('fill', 'white')
 
-    for (let i = 0; i < allDivs.length; i++) {
-        allDivs[i].style.color = 'whitesmoke'
+    if (changeMode.textContent == 'Change Dark') {
+
+        changeMode.textContent = 'Change Light'
+        themeColor.style.backgroundColor = '#202124'
+        themeColor.style.color = 'whitesmoke'
+        footerDiv.style.color = 'whitesmoke'
+        searchButton.style.backgroundColor = '#303134'
+        randomButton.style.backgroundColor = '#303134'
+        searchButton.style.color = '#e1e3e6'
+        randomButton.style.color = '#e1e3e6'
+        footer.style.backgroundColor = 'black'
+        svg.setAttribute('fill', 'white')
+
+        for (let i = 0; i < allDivs.length; i++) {
+            allDivs[i].style.color = 'whitesmoke'
+        }
+    } else {
+
+        changeMode.textContent = 'Change Dark'
+        themeColor.style.backgroundColor = 'white'
+        themeColor.style.color = 'black'
+        footerDiv.style.color = 'black'
+        searchButton.style.backgroundColor = '#f8f9fa'
+        randomButton.style.backgroundColor = '#f8f9fa'
+        searchButton.style.color = 'black'
+        randomButton.style.color = 'black'
+        footer.style.backgroundColor = 'white'
+        svg.setAttribute('fill', 'black')
+        console.log('error')
+
+        for (let i = 0; i < allDivs.length; i++) {
+            allDivs[i].style.color = 'black'
+        }
     }
+
 
 })
