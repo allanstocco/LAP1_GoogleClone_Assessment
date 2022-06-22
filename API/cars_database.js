b0db2584b750f73a592a497a5b10e51d4221cb06
allanstocco <allanstocco@hotmail.com>
2022-06-22 17:17:05 +0100

    Finish filling databases

--- API/cars_database.js
+++ API/cars_database.js
@@ -25,29 +25,29 @@ const cars = [
         info: "Buy or finance your next used car online and choose home delivery or collection. Sell your car without haggling and get an instant offer. No hidden fees."
     },
     {
-        header: "Cazoo: The better way to buy and sell a used car online in the ...",
-        url: 'https://www.cazoo.co.uk/',
-        info: "Buy or finance your next used car online and choose home delivery or collection. Sell your car without haggling and get an instant offer. No hidden fees."
+        header: "Auto Trader UK - New and Used Cars For Sale",
+        url: 'https://www.autotrader.co.uk/',
+        info: "Find your next car with Auto Trader UK, the official #1 site to buy and sell new and used cars. Over 400000 cars online. Simple, easy, quick!"
     },
     {
-        header: "Cazoo: The better way to buy and sell a used car online in the ...",
-        url: 'https://www.cazoo.co.uk/',
-        info: "Buy or finance your next used car online and choose home delivery or collection. Sell your car without haggling and get an instant offer. No hidden fees."
+        header: "Cargurus: Used Car Search - Most Cars Available Online",
+        url: 'https://www.cargurus.co.uk/',
+        info: "Use Custom Sorting and Filters. Find Great Deals Near You! Don't Miss Out. Quickly Filter by Price, Mileage, Trim, Deal Rating and More. Ratings You Can Trust. Millions of Listings. Dealer Reviews. Market Price Analysis. Trade-In Value. Price Alerts. Unbiased Deal Ratings."
     },
     {
-        header: "Cazoo: The better way to buy and sell a used car online in the ...",
-        url: 'https://www.cazoo.co.uk/',
-        info: "Buy or finance your next used car online and choose home delivery or collection. Sell your car without haggling and get an instant offer. No hidden fees."
+        header: "Cars Of London",
+        url: 'https://www.arnoldclark.com/used-cars',
+        info: "Cars Of London is a used car dealer in London stocking a wide range of second hand cars at great prices. Visit us today for affordable used cars in London."
     },
     {
-        header: "Cazoo: The better way to buy and sell a used car online in the ...",
-        url: 'https://www.cazoo.co.uk/',
-        info: "Buy or finance your next used car online and choose home delivery or collection. Sell your car without haggling and get an instant offer. No hidden fees."
+        header: "Vehicle Tracking Business - Automobile Auto",
+        url: 'https://www.gigapromo.co.uk/',
+        info: "Search Vehicle Tracking Business. Save now at GigaPromo! Always Sale. Cheap Prices. Full Offer. Simple Search. Compare Simply. Compare Online. Large Selection. The Best Price. Save Online. Services: Compare, Search, Find Products, Many Offers."
     },
     {
-        header: "Cazoo: The better way to buy and sell a used car online in the ...",
-        url: 'https://www.cazoo.co.uk/',
-        info: "Buy or finance your next used car online and choose home delivery or collection. Sell your car without haggling and get an instant offer. No hidden fees."
+        header: "Different Class Cars: Used Cars London, Used Car Dealer in ...",
+        url: 'https://www.dcc.co.uk/',
+        info: "Different Class Cars is a used car dealer in London stocking a wide range of second hand cars at great prices. Visit us today for affordable used cars in ..."
     },
 
 ]
--- API/poney_database.js
+++ API/poney_database.js
@@ -1,53 +1,53 @@
 const poneys = [
     {
+        header: "pony | small horse - Encyclopedia Britannica",
+        url: 'https://www.britannica.com/animal/ponys',
+        info: "pony, any of several breeds of small horses standing less than 14.2 hands (147 cm, or 58 inches) high and noted for gentleness and endurance."
+    },
+    {
+        header: "Amazing Facts about Ponies | OneKindPlanet Animal ...",
+        url: 'https://onekindplanet.org',
+        info: "Ponies are generally considered to be 14.2 hands or under. Horses and ponies are measured in 'hands' with each 'hand' measuring 4 inches. Ponies height is ..."
+    },
+    {
+        header: "The Stone Poneys': The Early Folk-Rock Adventures Of Linda ...",
+        url: 'https://www.udiscovermusic.com/stories/stone-poneys-stone-poneys-album/',
+        info: "The persuasive folk-rock of the Stone Poneys was first on disc on their self-titled debut album, released by Capitol on January 30, 1967."
+    },
+    {
+        header: "Caring For Your Horse or Pony | RSPCA",
+        url: 'https://www.rspca.org.uk',
+        info: "There's no single 'perfect' way to care for all horses and ponies, because every animal and every situation is different. It's up to you how you look after them ..."
+    },
+    {
+        header: "Horses | Animal care and control - Thurrock Council",
+        url: 'https://www.thurrock.gov.uk',
+        info: "To report cruelty or an animal in distress. Horses on the Mardyke Valley receive regular welfare checks from the RSPCA, who are in contact with the horse owners ..."
+    },
+    {
+        header: "Estride Suite - Equine Performance Technology",
+        url: 'https://www.estride.store/?gclid=CjwKCAjw-8qVBhANEiwAfjXLrkVg3cXx5EsZrMXKN08TWdjCKNlFf40PKh0TJ-jps1Wt0BfypcYxCxoCGvMQAvD_BwE',
+        info: "Wearable for riders to measure their hand steadiness while horse riding. Award winning technology aid to ensure neutral hand position while riding. Cloud Based. Ships worldwide. Any Equine Discipline."
+    },
+    {
+        header: "The Stone Poneys - Concert - Setlist.fm",
+        url: 'https://www.setlist.fm/setlists/the-stone-poneys-53d6ab91.html',
+        info: "Get The Stone Poneys setlists - view them, share them, discuss them with other The Stone Poneys fans for free on setlist.fm!"
+    },
+    {
+        header: "Horse passports: semi-feral ponies - GOV.WALES",
+        url: 'https://gov.wales/horse-passports-semi-feral-ponies',
+        info: "Owners of horses must have an ID document or passport for each horse. When they apply for a passport, the animal must be micro-chipped. Wild or ..."
+    },
+    {
+        header: "Run a animal refuge - Volunteer with horses",
+        url: 'https://tenerifehorserescue.com/how-to-work-with-animals/?gclid=CjwKCAjw-8qVBhANEiwAfjXLroVpTu3a70iWczRzdMnm_84ZvQ-coaUGbefFVqdFpkg9PdNSCHABixoCQUMQAvD_BwE',
+        info: "What is the correct attitude to take and the most frequent jobs with animals to not avoid. We are totally run by volunteers and our aim is to offer homes to all horses and animals."
+    },
+    {
         header: "Poney",
         url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://en.normandie-tourisme.fr/sports-and-leisure-activities/poney-club-poney-pour-tous/',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://www.gettyimages.fi/photos/poney-des-shetland',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
-    },
-    {
-        header: "Poney",
-        url: 'https://fr.wikipedia.org/wiki/Poney',
-        info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
+        info: "First ponei in this task"
     },
     
 ]
