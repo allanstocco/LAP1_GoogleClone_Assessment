da81921cca64d45b507085cb85d176ad8d242ad4
VAOkolo <vincentaokolo@outlook.com>
2022-06-22 11:01:16 +0100

    changes

--- API/database.js
+++ API/database.js
@@ -1,8 +1,18 @@
-const data = [
-    'https://fr.wikipedia.org/wiki/Poney',
-    'https://en.normandie-tourisme.fr/sports-and-leisure-activities/poney-club-poney-pour-tous/',
-    'https://www.gettyimages.fi/photos/poney-des-shetland'
-]
+const data = [{
+    header: "Poney",
+    url: 'https://fr.wikipedia.org/wiki/Poney',
+    info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
+},
+{
+    header: "Poney",
+    url: 'https://en.normandie-tourisme.fr/sports-and-leisure-activities/poney-club-poney-pour-tous/',
+    info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
+},
+{
+    header: "Poney des-shetland",
+    url: 'https://www.gettyimages.fi/photos/poney-des-shetland',
+    info: "asidhksdhkasdjkaksdjadkajhsdjhasjddaksuhdkajdshkjasdkjadsjkskadakdksadksjdakddksbdkbdkadsbdjk"
+}   ]
 
 
 module.exports = data
