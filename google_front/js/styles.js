9910ba23dd15fcfc18da50ed1257aaf36d93c626
allanstocco <allanstocco@hotmail.com>
2022-06-22 23:07:42 +0100

    Dark theme improvements, file reorganization

--- google_front/js/styles.js
+++ google_front/js/styles.js
@@ -3,8 +3,10 @@ const allDivs = document.getElementsByTagName('a');
 const changeMode = document.querySelector('#changeMode');
 const searchButton = document.querySelector('.search-button');
 const randomButton = document.querySelector('.random-button');
+const logoTheme = document.querySelector('#logo_google')
 const footer = document.querySelector('.upperFooter');
 const footerDiv = document.querySelector('.location');
+const input = document.querySelector('.field_A');
 const svg = document.querySelector('#svg');
 
 changeMode.addEventListener('click', (e) => {
@@ -12,6 +14,7 @@ changeMode.addEventListener('click', (e) => {
     if (changeMode.textContent == 'Change Dark') {
 
         changeMode.textContent = 'Change Light'
+        logoTheme.src = '../images/google_logo_dark.png'
         themeColor.style.backgroundColor = '#202124'
         themeColor.style.color = 'whitesmoke'
         footerDiv.style.color = 'whitesmoke'
@@ -19,7 +22,9 @@ changeMode.addEventListener('click', (e) => {
         randomButton.style.backgroundColor = '#303134'
         searchButton.style.color = '#e1e3e6'
         randomButton.style.color = '#e1e3e6'
-        footer.style.backgroundColor = 'black'
+        footerDiv.style.borderBottom = '1px solid #212327'
+        footer.style.backgroundColor = '#181818'
+        input.style.background = '#202124'
         svg.setAttribute('fill', 'white')
 
         for (let i = 0; i < allDivs.length; i++) {
@@ -28,19 +33,21 @@ changeMode.addEventListener('click', (e) => {
     } else {
 
         changeMode.textContent = 'Change Dark'
+        logoTheme.src = '../images/googlelogo_color_272x92dp.png'
         themeColor.style.backgroundColor = 'white'
         themeColor.style.color = 'black'
-        footerDiv.style.color = 'black'
+        footerDiv.style.color = '#70757a'
         searchButton.style.backgroundColor = '#f8f9fa'
         randomButton.style.backgroundColor = '#f8f9fa'
         searchButton.style.color = 'black'
         randomButton.style.color = 'black'
+        footerDiv.style.borderBottom = '1px solid #dadce0'
         footer.style.backgroundColor = 'white'
+        input.style.background = '#fff'
         svg.setAttribute('fill', 'black')
-        console.log('error')
 
         for (let i = 0; i < allDivs.length; i++) {
-            allDivs[i].style.color = 'black'
+            allDivs[i].style.color = '#70757a'
         }
     }
 
