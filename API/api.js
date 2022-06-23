11ec634cfffe8345ca6801792f786d634713daec
allanstocco <allanstocco@hotmail.com>
2022-06-23 08:45:42 +0100

    latest changes

--- API/api.js
+++ API/api.js
@@ -8,30 +8,33 @@ const cars = require('./cars_database')
 
 const arr = poney.concat(cars);
 
-console.log(arr);
-
 
 app.use(express.json())
 app.use(cors())
 
-
 app.get('/ponei', (req, res) => {
-    res.send(poney)
-    
+  res.send(poney)
+
 })
 
 app.get('/cars', (req, res) => {
-    res.send(cars)
+  res.send(cars)
 })
 
 function getRandomItem() {
-    let rand = Math.floor(Math.random() * arr.length + 1)
-    return arr[rand];
-  }
+  let rand = Math.floor(Math.random() * arr.length + 1)
+  return arr[rand];
+}
 
 app.get('/search/random', (req, res) => {
-    res.send(getRandomItem());
-  })
+  res.send(getRandomItem());
+})
+
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  res.send([{ header: 'Could not find any results for this search' }]);
+});
 
 app.listen(3000, () => console.log(`Listening port http://0.0.0.0:3000`))
 
