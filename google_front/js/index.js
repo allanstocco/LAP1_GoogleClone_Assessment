fbf619b2d9793e102271c48ea7d107a8eb36286e
allanstocco <allanstocco@hotmail.com>
2022-06-23 17:31:43 +0100

    toggle  api form and local data form working

--- google_front/js/index.js
+++ google_front/js/index.js
@@ -1,13 +1,37 @@
 const googleSearch = document.querySelector('.search-button');
 const randomSearch = document.querySelector('#random-button');
 const submitQuery = document.querySelector('#submit_query')
+const database_request = document.querySelector('#database_request');
+const api_request = document.querySelector('#api_request');
+const data_id = document.querySelector('#data_id');
+const api_id = document.querySelector('#api_id');
+
+
 
 
 
 // googleSearch.addEventListener('click', getAllResults);
 
+data_id.addEventListener('click', (e) => {
+    database_request.style.display = 'block';
+    api_request.style.display = 'none';
+    data_id.style.backgroundColor = '#4285f4';
+    data_id.style.color = 'whitesmoke';
+    api_id.style.backgroundColor = '#fff';
+    api_id.style.color = 'black';
+})
 
-randomSearch.addEventListener('click', async (e) =>{
+api_id.addEventListener('click', (e) => {
+    api_request.style.display = 'block';
+    database_request.style.display = 'none';
+    data_id.style.backgroundColor = '#fff';
+    data_id.style.color = 'black';
+    api_id.style.backgroundColor = '#4285f4';
+    api_id.style.color = 'whitesmoke';
+})
+
+
+randomSearch.addEventListener('click', async (e) => {
     e.preventDefault();
     console.log(e)
     let response = await fetch('http://0.0.0.0:3000/search/random');
--- google_front/js/search.js
+++ google_front/js/search.js
@@ -2,29 +2,77 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const query = urlParams.get('q')
 
+const api_query = urlParams.get('api_q')
+
 const searchResultArea = document.querySelector('#searchresultsarea');
 
 
 async function fetchData(query) {
 
-    // let response = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyC527TEPWQJWEGg7bffb2zsvIbWFnxFRDw&cx=abf820f0c36deb757&q=${query}`);
-    let response = await fetch(`http://localhost:3000/${query}`);
-    let data = await response.json();
-    // console.log(data.items);
-    // appendResults(data.items);
 
-    console.log(data);
+    let response = await fetch(`http://0.0.0.0:3000/${query}`);
+    let data = await response.json();
+
     appendResults(data);
-    
     relatedSearches(query);
 }
+
+async function fetchAPI(api_query) {
+
+    let response = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyC527TEPWQJWEGg7bffb2zsvIbWFnxFRDw&cx=abf820f0c36deb757&q=${api_query}`);
+    let data = await response.json();
+
+    appendApiResults(data.items);
+    relatedSearches(api_query);
+}
+
+
+function appendApiResults(data) {
+    data.forEach(renderApiResult)
+}
+
 
 function appendResults(data) {
     data.forEach(appendResult)
 }
 
-function appendResult(itemData){
-    
+
+function renderApiResult(apiData) {
+    const searchResultArea = document.querySelector('#searchresultsarea');
+    console.log(searchResultArea);
+    const container = document.createElement('div');
+    const header = document.createElement('h2');
+    const link = document.createElement('a');
+    const headerLink = document.createElement('a');
+    const text = document.createElement('p');
+    container.setAttribute('class', 'searchresult');
+    // container.setAttribute('id', `searchresult-${parseInt(apiData[0] + 1)}`)
+
+    link.textContent = apiData.displayLink;
+    link.href = apiData.link;
+    text.textContent = apiData.snippet;
+
+    headerLink.href = apiData.link;
+    headerLink.textContent = apiData.title;
+
+    header.appendChild(headerLink);
+
+
+
+    // console.log(header, link, text);
+
+    container.appendChild(header);
+    container.appendChild(link);
+    container.appendChild(text);
+
+    console.log(container);
+    searchResultArea.append(container);
+
+}
+
+
+function appendResult(itemData) {
+
     // console.log(itemData.header);
     const searchResultArea = document.querySelector('#searchresultsarea');
     console.log(searchResultArea);
@@ -34,17 +82,7 @@ function appendResult(itemData){
     const headerLink = document.createElement('a');
     const text = document.createElement('p');
     container.setAttribute('class', 'searchresult');
-    // container.setAttribute('id', `searchresult-${parseInt(itemData[0] + 1)}`)
 
-    //API variables
-    // link.textContent = itemData.displayLink;
-    // link.href = itemData.link;
-    // text.textContent = itemData.snippet;
-
-    // headerLink.href = itemData.link;
-    // headerLink.textContent = itemData.title;
-
-    //comment out and uncomment above if you want api access
     link.textContent = itemData.url;
     link.href = itemData.url;
     text.textContent = itemData.info;
@@ -67,7 +105,7 @@ function appendResult(itemData){
 
     //error handling for when query doesn't match our internal database
     let errorHeader = "Could not find any results for this search"
-    if(headerLink.textContent == errorHeader){
+    if (headerLink.textContent == errorHeader) {
         let pagebar = document.querySelector('.pagebar');
         let relatedsearches = document.querySelector('.relatedsearches')
         headerLink.href = "../google_front/error.html"
@@ -77,7 +115,7 @@ function appendResult(itemData){
 }
 
 
-async function relatedSearches(query){
+async function relatedSearches(query) {
     let relatedSearches = document.querySelector('#relatedsearches-header');
     relatedSearches.textContent = `Search results related to ${query}`;
 
@@ -88,7 +126,12 @@ async function relatedSearches(query){
 }
 
 
-fetchData(query)
+if (queryString.includes('api')) {
+    fetchAPI(api_query)
+} else {
+    fetchData(query)
+}
+
 
 
 exports = {
